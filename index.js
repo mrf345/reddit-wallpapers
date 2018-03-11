d388dcab6a39dfd47a24f42f395bc4e306cea8d3
mrf3 <mrf345@gmail.com>
2018-03-11 15:06:38 +0800

    fix settings

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ export default function redditWallpapers (options={}) {
     self.options = { // default options, if not provided
         id: options.id || '.reddit', // id or css class of element to wallpaper to
         category: options.category || self.categories, // Array of categories to choose from randomly
-        duration: options.duration || 10000, // duration of 5 seconds
-        aDuration: options.aDuration * 1000 || 1000, // transsition animation duration
+        duration: options.duration * 1000 || 30000, // duration of 30 seconds
+        aDuration: options.aDuration * 1000 || 500, // transsition animation duration
         limit: options.limit || 30, // limit of json items
         timeout: options.timeout * 1000 || 5000, // request timeout in seconds
         overlay: options.overlay || 'rgba(0,0,0,0.7)', // black transparnt overlay color
@@ -59,7 +59,7 @@ export default function redditWallpapers (options={}) {
             let image = self.data[self.index].data.url
             CheckImg(image).then(() => {
                 $(self.options.id).stop().animate(
-                    self.options.isAnimated === 'true' ? {opacity: 0.4} : {opacity: 1},
+                    self.options.isAnimated === 'true' ? {opacity: 0.3} : {opacity: 1},
                     self.options.aDuration,
                     function () {
                         let style = {
